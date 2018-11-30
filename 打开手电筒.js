330598923056204fa1eb3d5fe3b024fa627dac3b
miles-cui <miles_cui@tom.com>
2018-11-30 13:36:27 +0800

    测试2

--- 打开手电筒.js
+++ 打开手电筒.js
@@ -2,6 +2,10 @@
  1、手机必须解锁。
  2、手机任务栏中五个图标一行。
  3、第三个图标是手电筒。
+    图标排列
+    1、移动数据
+    2、WiFi
+    3、飞行模板
  */
 
 //屏幕网络按钮位置
@@ -11,14 +15,20 @@ let NetButtonY = 456;
 let devices = Device.connectAll();
 //获得主设备
 let device = Device.getMain();
+//第一次操作
 //下滑
 device.swipe([[555,18,30],[555,400,100],[555,900,100]]);
-//打开手电
+//点击第一个图标（打开）
 device.click(538, 456, sigmaConst.STATE_PRESS);
 delay(3000);
-//关闭手电
+//再次点击第一个图标（关闭）
 device.click(538, 456, sigmaConst.STATE_PRESS);
+
+
+
+
 //按下返回键并弹起
 device.send(sigmaConst.KEY_BACK, sigmaConst.STATE_PRESS);
 //按下主页键并弹起
 device.send(sigmaConst.KEY_HOME, sigmaConst.STATE_PRESS);
+
